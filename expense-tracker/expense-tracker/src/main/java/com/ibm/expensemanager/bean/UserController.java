044a1f7f6673b2d8c6b2ecd4ba972a6af88e371a
vasu upadhayay <vasuupadhayaytech@gmail.com>
2020-03-25 13:52:07 +0530

    Add files via upload

---
+++ expense-tracker/expense-tracker/src/main/java/com/ibm/expensemanager/bean/UserController.java
@@ -0,0 +1,34 @@
+package com.ibm.expensemanager.bean;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
+import com.ibm.expensemanager.bean.UserTransaction;
+import com.ibm.expensemanager.bean.UserRepository;
+
+@RestController
+//@RequestMapping("/user-app")
+public class UserController {
+
+	@Autowired
+	UserRepository repo;
+	
+	@RequestMapping("/users")
+	Iterable<UserTransaction> getUsers(){
+		return repo.findAll();
+	}
+	
+	@RequestMapping("/users/date/{date}")
+	List<Match> getDate(@PathVariable Date date) {
+		return repo.findByMatchMonthAndMatchDay(date);
+	}
+}
